0c9f370253f9eb48c5861883f8b0d0c34afca517
Barbaraeaston <46596557+Barbaraeaston@users.noreply.github.com>
2019-05-22 16:28:31 +0200

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -4,6 +4,9 @@ import random
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
+    Voor onze code zie recommendersystems notebook
+    """
+    """
     Returns n recommendations as a list of dicts.
     Optionally takes in a user_id, business_id and/or city.
     A recommendation is a dictionary in the form of:
